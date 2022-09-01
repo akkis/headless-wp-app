02a7b6298ef097b112c8eaeaa1d695d2dfe29394
Akis Laoutaris <alaoutaris@nelios.com>
2022-09-02 00:04:01 +0300

    Add SASS support

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
+const path = require('path');
+
 if (!process.env.WORDPRESS_API_URL) {
   throw new Error(`
     Please provide a valid WordPress instance URL.
     Add to your environment variables WORDPRESS_API_URL.
-  `)
+  `);
 }
 
 module.exports = {
